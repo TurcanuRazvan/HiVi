7c1635c25f59ddd94ba080ac19cfa322fce0c350
madalina11 <madalina11_01@yahoo.com>
2017-11-28 17:03:44 +0200

    update

--- asset/script/index.js
+++ asset/script/index.js
@@ -2,12 +2,26 @@
 
 	document.getElementById('despreBtn').addEventListener('click', function() {
 		document.getElementsByClassName('list-history')[0].classList.add('displayNone');
+		document.getElementsByClassName('create-graphics')[0].classList.add('displayNone');
 		document.getElementsByClassName('despre')[0].classList.remove('displayNone');
+		document.getElementsByClassName('footer')[0].classList.remove('displayNone');
 	});
 	document.getElementById('historyBtn').addEventListener('click', function() {
 		document.getElementsByClassName('despre')[0].classList.add('displayNone');
 		document.getElementsByClassName('footer')[0].classList.add('displayNone');
+		document.getElementsByClassName('create-graphics')[0].classList.add('displayNone');
+		document.getElementsByClassName('despre')[0].classList.add('displayNone');
 		document.getElementsByClassName('list-history')[0].classList.remove('displayNone');
+	});
+	document.getElementById('graphicsBtn').addEventListener('click', function() {
+		document.getElementsByClassName('despre')[0].classList.add('displayNone');
+		document.getElementsByClassName('footer')[0].classList.add('displayNone');
+		document.getElementsByClassName('list-history')[0].classList.add('displayNone');
+		document.getElementsByClassName('despre')[0].classList.add('displayNone');
+		document.getElementsByClassName('create-graphics')[0].classList.remove('displayNone');
 	});
 
 })();
+
+
+// graphicsBtn
